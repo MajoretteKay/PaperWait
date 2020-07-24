ac2cb12bb4d429cdb5d2afa8655774b6418a3ee1
MajoretteKay <keolecki@gmail.com>
2020-07-23 22:38:55 -0400

    site layout added

--- src/engine/render.js
+++ src/engine/render.js
@@ -1,16 +1,24 @@
 function renderSiteHTML() {
     let string =  
-    `<div class="hub">
+    `<header>PaperWait Logo Here</header>
+    <div class="container">
+        <button>Monthly Drawing Challenges</button>
+        <button>Lesson Prompts</button>
+        <button></button>
+        <button></button>
+    </div>
+    <div class="hub">
         <div class="innerhub top">
         top
         </div>
-        <div class="innerhub middle">
+        <div class="innerhub app middle">
         middle
         </div>
         <div class="innerhub bottom">
         bottom
         </div>
-    </div>`;
+    </div>
+    <footer>link to my site</footer>`;
     
     $('#root').append(string);
     
